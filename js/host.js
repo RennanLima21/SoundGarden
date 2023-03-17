40ee4131ed81b7123a1af00d1ba21e9121d3502c
Rennan Lima <rennanlima@hotmail.com>
2023-03-17 14:32:22 -0400

    GET byID add

--- js/host.js
+++ js/host.js
@@ -1,37 +1,66 @@
-export const BASE_URL = 'https://soundgarden-api.vercel.app/';
+export const BASE_URL = "https://soundgarden-api.vercel.app/";
 
+/* Const metodo GET  */
 export async function getApiData({ pathUrl }) {
-  const URL = pathUrl ? `${BASE_URL}/${pathUrl}` : BASE_URL; // ternario__(Verdae_?)__(Falso_:)
+  const URL = pathUrl ? `${BASE_URL}${pathUrl}` : BASE_URL; // ternario__(Verdae_?)__(Falso_:)
 
   const response = await fetch(URL, {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
   });
 
   return await response.json();
-};
 
+}
+/* GET byID */
+export async function getById({ pathUrl, id }) {
+  const URL = pathUrl ? `${BASE_URL}${pathUrl}/${id}` : BASE_URL; // ternario__(Verdae_?)__(Falso_:)
+
+  const response = await fetch(URL, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  return await response.json();
+}
+
+// POST
 /* pathUrl, body (tranformado os argumentos em  {Objeto} para organizar a exibição de acordo com sua necessidade) */
 export async function postApiData({ pathUrl, body }) {
   const URL = pathUrl ? `${BASE_URL}${pathUrl}` : BASE_URL;
 
   const response = await fetch(URL, {
-    method: 'POST',
+    method: "POST",
     body: JSON.stringify(body),
-    headers: { 'Content-Type': 'application/json' },
+    headers: { "Content-Type": "application/json" },
   });
 
   return await response.json();
-};
+}
 
-export async function putApiData ({ pathUrl, id, body }) {
+//VERIFICAR
+/* Const para PUT (editar evento) VER SE ESTA CERTA */
+export async function putApiData({ pathUrl, id, body }) {
   const URL = pathUrl && id ? `${BASE_URL}${pathUrl}/${id}` : BASE_URL;
 
   const response = await fetch(URL, {
-    method: 'PUT',
+    method: "PUT",
     body: JSON.stringify(body),
-    headers: { 'Content-Type': 'application/json' },
+    headers: { "Content-Type": "application/json" },
   });
 
   return await response.json();
-};
+}
+
+//VERIFICAR
+/* Const para DELETE (deletar evento)  VER SE ESTA CERTA */
+export async function deleteApiData({ pathUrl, id, body }) {
+  const URL = pathUrl && id ? `${BASE_URL}${pathUrl}/${id}` : BASE_URL;
+
+  const response = await fetch(URL, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  return await response.json();
+}
